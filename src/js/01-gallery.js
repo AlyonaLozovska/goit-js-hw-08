9cf20aff9c14a7ec14b55492d408df63cc1aaaf1
Alyona <alozovska1504@gmail.com>
2021-12-27 23:02:48 +0200

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+// import Simplelightbox from "simplelightbox";
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+// import { galleryItems } from './gallery-items';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryImg = document.querySelector('.gallery');
+
+const imagesMarkup = galleryItems
+.map(({preview,original,description}) => 
+`<div class = "gallery__item">
+<a class = "gallery__link" href = "${original}">
+<img class = "gallery__image"
+  src = "${preview}"
+  data-source = "${original}"
+  alt = "${description}"/>
+</a></div>`).join("");
+
+galleryImg.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from "lodash.throttle";
+import '../css/common.css';
+import '../css/feedback.css';
 
+const STORAGE_KEY = 'feedback';
+
+const formData ={};
+
+const refs = {
+    form: document.querySelector('.js-feedback'),
+    textarea: document.querySelector('.js-feedback textarea'),
+};
+
+refs.form.addEventListener('submit', onFormsubmit);
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+refs.form.addEventListener('input', e => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(FORM_STATE, JSON.stringify(formData));
+    console.log(formData);
+});
+
+populateTextarea();
+
+function onFormsubmit(evt) {
+    evt.prevenDefault();
+    console.log('Отправляем форму');
+    evt.curentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function onTextareaInput(evt) {
+    const message = evt.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(message));
+};
+
+function populateTextarea() {
+    const savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (savedMessage) {
+        console.log(savedMessage);
+        refs.textarea.value = savedMessage;
+    }
+};
